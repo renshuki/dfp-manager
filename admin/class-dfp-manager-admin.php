37c92f713d18a8bf4c0ba051c6ab172ef00cee43
renshuki <renshuki@mail.com>
2017-07-07 00:05:59 +0900

    Fix PHP Notice

--- admin/class-dfp-manager-admin.php
+++ admin/class-dfp-manager-admin.php
@@ -223,7 +223,7 @@ class Dfp_Manager_Admin {
     $mg_post_type = 'ad-slot';
     global $post;
 
-    if($post->post_type == $mg_post_type){
+    if(isset($post->post_type) == $mg_post_type){
       echo "
         <style type='text/css'>
           #misc-publishing-actions,
