49c7dcaf611878d2d3414508a3d20ccd9c8addec
renshuki <renshuki@mail.com>
2017-07-13 17:51:26 +0900

    Secure dfp_manager_keys directory

--- includes/class-dfp-manager-activator.php
+++ includes/class-dfp-manager-activator.php
@@ -34,6 +34,7 @@ class Dfp_Manager_Activator {
 
     // Create upload folder for JSON keys
     $activator->create_upload_folder(constant("UPLOAD_FOLDER"));
+    $activator->create_htaccess_file(constant("UPLOAD_FOLDER"));
 	}
 
   function create_upload_folder($folder_name) {
@@ -42,6 +43,19 @@ class Dfp_Manager_Activator {
 
     if ( ! file_exists( $dfp_manager_dirname ) ) {
         wp_mkdir_p( $dfp_manager_dirname );
+    }
+  }
+
+  function create_htaccess_file($folder_name) {
+    $filename = ".htaccess";
+    $upload_dir = wp_upload_dir(null, false);
+    $dfp_manager_dirname = $upload_dir['basedir'].'/'. $folder_name;
+    $file_path = $dfp_manager_dirname . "/" . $filename;
+    
+    if ( ! file_exists($file_path) ) {
+      $file = fopen($file_path, "w");
+      fwrite($file, "Deny from all");
+      fclose($file);
     }
   }
 
