478fa745a044c8dfd323231cb55ab442e9f3df57
renshuki <renshuki@mail.com>
2017-08-30 11:56:40 +0900

    Remove priority on admin_init

--- includes/class-dfp-manager.php
+++ includes/class-dfp-manager.php
@@ -162,7 +162,7 @@ class Dfp_Manager {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );	
-		$this->loader->add_action( 'admin_init', $plugin_admin, 'init', 100 );
+		$this->loader->add_action( 'admin_init', $plugin_admin, 'init' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'custom_post' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'custom_taxonomy' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'settings' );
